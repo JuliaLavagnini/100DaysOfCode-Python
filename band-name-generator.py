5812439914d862fb0364e2fb83d659f8b578e4e4
Julia Lavagnini Maia <65735429+JuliaLavagnini@users.noreply.github.com>
2022-10-14 11:50:59 +0100

    Delete band-name-generator.py

--- band-name-generator.py
+++
@@ -1,8 +0,0 @@
-
-print("Hello welcome to band names. Let's go!! ")
-
-city = input("Where did you grow up?\n")
-pet = input("What is the name of your favorite pet?\n")
-Band = "Your band name could be " + city + " " + pet
-
-print(Band)
