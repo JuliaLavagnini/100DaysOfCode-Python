9d41aecd4dbdf84047678a2a14b14b8a88472845
Julia Lavagnini Maia <65735429+JuliaLavagnini@users.noreply.github.com>
2022-10-14 11:41:11 +0100

    Add files via upload

---
+++ band-name-generator.py
@@ -0,0 +1,8 @@
+
+print("Hello welcome to band names. Let's go!! ")
+
+city = input("Where did you grow up?\n")
+pet = input("What is the name of your favorite pet?\n")
+Band = "Your band name could be " + city + " " + pet
+
+print(Band)
